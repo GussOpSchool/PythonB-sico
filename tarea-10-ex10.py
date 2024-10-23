df79d7dfd7c14dd5a4cb25bb0d25f646551076f6
GussopSchool <gtecchio20969@iesjoanramis.org>
2024-10-23 12:27:59 +0200

    Ejercicios 10 y 11

---
+++ tarea-10-ex10.py
@@ -0,0 +1,3 @@
+nombre = str(input("Dime tu primer nombre: "))
+apellido = str(input("Y tus apellidos: "))
+print("Hola, {} {}".format(nombre, apellido))
---
+++ tarea-10-ex11.py
@@ -0,0 +1,8 @@
+edad = int(input("Dime tu edad: "))
+
+if edad == 18:
+    print("Tienes justo 18 años")
+elif edad < 18:
+    print("Eres menor de edad")
+elif edad > 18:
+    print("Eres mayor de edad")
